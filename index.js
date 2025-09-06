cd51b3e1323cd3db9f7d1a56aaf0293200b0b3f9
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-09-06 08:33:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://snt-frncs.vercel.app/'],
+  origin: ['http://localhost:3000', 'https://snt-frncs.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
