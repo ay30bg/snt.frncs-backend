3627a6066bfc06c84555b48ba5012946598e4210
retro.dev <117005347+ay30bg@users.noreply.github.com>
2025-09-08 20:34:58 +0100

    Update index.js

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 // Routes
 app.use('/api/', authRoutes);
-app.use('/api/payment', paymentRoutes);
+app.use('/api/paystack', paymentRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'SNTFRNCS API' });
